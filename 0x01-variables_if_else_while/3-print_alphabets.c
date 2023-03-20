826b498eff66993752e93252cf93ac2e6922547c
arcade <arcadeizere24@gmail.com>
2023-03-20 13:34:56 -0700

    asdf

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-
 /**
- * main - Prints the alphabet in lowercase, except for q and e.
- *
- * Return: Always 0.
+ * main - Prints the alphabet in lowercase, then in uppercase.
+(*
+ * Return: 0 on success
  */
 int main(void)
 {
-	char letter;
+	char c = 'a';
+	char d = 'A';
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	while (c <= 'z')
 	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
+		putchar(c);
+		c++;
 	}
-
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
 	putchar('\n');
-
 	return (0);
+
 }
