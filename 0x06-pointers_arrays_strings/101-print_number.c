9403534326bbf3079f7054e147f1d1e6bd24f884
arcade <arcadeizere24@gmail.com>
2023-03-31 10:58:29 -0700

    sdfghjk

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 #define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 #define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
