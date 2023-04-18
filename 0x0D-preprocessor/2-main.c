91b5221cdd8d73c8b55e5f19a83b5af4df67b84c
arcade <arcadeizere24@gmail.com>
2023-04-18 14:50:48 -0700

    fghj

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#ifndef __2_MAIN_C__
+#define __2_MAIN_C__
+
+#define NAME __FILE__
+
+#include <stdio.h>
+
+/**
+  * main - Print name of file.
+  *
+  * Return: 0.
+  */
+
+int main(void)
+{
+	printf("%s\n", NAME);
+	return (0);
+}
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef __3_FUNCTION_LIKE_MACRO_H__
+#define __3_FUNCTION_LIKE_MACRO_H__
+
+#define ABS(X) ((X) < 0 ? -(X) : (X))
+
+#endif
