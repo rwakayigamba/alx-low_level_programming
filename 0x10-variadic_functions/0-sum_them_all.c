443fd17d50125ace889c65ce3bf99dff6b3ac65c
arcade <arcadeizere24@gmail.com>
2023-04-21 14:05:40 -0700

    dfgh

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+  * sum_them_all - Sums up all passed numbers.
+  * @n: Number of parameters.
+  * Return: Sum.
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, int);
+	}
+
+	va_end(valist);
+
+	return (sum);
+}
