bf21c3fb56df1ff3fc0cc5ffd658b47a32c47fa9
arcade <arcadeizere24@gmail.com>
2023-04-28 18:07:31 -0700

    sdfghj

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,18 @@
-extern printf
+	extern printf
 
-section .text
-   global main
+	section .data
+msg:		db "Hello, Holberton", 0
+fmt:		db "%s", 10, 0
 
+	section .text
+
+	global main
 main:
-   push rbp
-
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
-
-section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0i
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax,0
+	ret
